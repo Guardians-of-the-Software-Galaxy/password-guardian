dbf545e6817889897b1bbb94bf4dc808b1e51425
nicholasbcarr <89881148+nicholasbcarr@users.noreply.github.com>
2023-02-16 06:52:37 -1000

    nope

--- password_guardian.py
+++ password_guardian.py
@@ -1,3 +1,14 @@
+from cryptography.fernet import Fernet
+
+# list vars to determine information
+key_control = 0  # determine if need to generate a key or if a key is already stored
+
+
+# Create the key unique to each user
+key = Fernet.generate_key()
+
+
+
 
 
 fp = open('data.txt', 'r')
