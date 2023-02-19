0747ec5dd249bf477a29c5f285ae46f9ab72f9e6
nicholasbcarr <89881148+nicholasbcarr@users.noreply.github.com>
2023-02-19 09:20:28 -1000

    login loop, create account loop, fernet tests, user interface all working

--- password_guardian.py
+++ password_guardian.py
@@ -2,40 +2,19 @@
 from password_guardian_driver import *
 
 
+
 if(login_request()):
     # read data.txt into a list
     print("Hello big butt!")
+    # call function to read encrypted data from data.txt into a list or array of credential objects !LEAVE ENCRYPTED!
 
 else:
     print("Would you like to create a password guardian? Enter (y) for YES, (n) for NO")
-    # call create account function
+    create_account()
+
+f = get_key()
+message = f.encrypt(b'Hello big butt!')
+print(message)
+print(f.decrypt(message))
         
 
-
-# key is generated
-#key = Fernet.generate_key()
-
-#with open('filekey.key', 'wb') as filekey:
-   #filekey.write(key)
-
-# value of key is assigned to a variable
-#f = Fernet(key)
-  
-# the plaintext is converted to ciphertext
-#with open('data.txt', 'rb') as file:
-    #original = file.readline()
-    #print(original)
-    #token = f.encrypt(original)
-    #print(token)
-    #print('='*20)
-    #d_token = f.decrypt(token)
-    #print(d_token)
-# display the ciphertext
-
-  
-# decrypting the ciphertext
-#d = f.decrypt(token)
-  
-# display the plaintext and the decode() method 
-# converts it from byte to string
-#print(d.decode())
--- password_guardian_driver.py
+++ password_guardian_driver.py
@@ -5,28 +5,68 @@ from cryptography.fernet import Fernet
 
 def login_request():
 
-    login_loop = True
-    x = True
+    login_loop = True     # controls the login loop
+    has_account = False   # returns if the user has an account
 
     while(login_loop):
 
         user_in = input('Do you have a login and password? Enter (y) for YES, Enter (n) for NO\n')
 
         if(user_in == 'y' or user_in == 'Y' or user_in == 'yes' or user_in == 'YES' or user_in == 'Yes'):
-            with open('filekey.key', 'rb') as filekey:
+            with open('filekey.key', 'rb') as filekey:   # here we get the key to test the login/password only
                 key = filekey.read()
                 f = Fernet(key)
-                login = input('Enter your username:\n').encode()
+                login = input('Enter your username:\n').encode()          # encode is to make string behave with Fernet
                 password = input('Enter your password:\n').encode()
 
-                x = f.encrypt(login)
+                x = f.encrypt(login)                     # here we need to read in the first  encrypted credential object and match encrypted values
                 y = f.encrypt(password)
-                print(x)
-                print(y)
+                print(x)                                 # testing
+                print(y)                                 # testing
                 login_loop = False
+                has_account = True
+                # compare the password/login to the first credential object in data.txt, if match return true
         else:
-            print("The user does not have an account. Please make an account.")
-            x = False
+            print("The user does not have an account. Please make an account.") 
+            has_account = False
             login_loop = False
 
-    return x
+    return has_account
+
+def create_account():                           # create account function, need to make sure a user cannot accidentally overwrite their account
+                                                # if a new key is generated and written to filekey, everything is essentially lost
+    create_loop = True
+
+    while(create_loop):
+
+        login = input("Enter a username for password guardian:\n").encode()  # encode is to make string behave with Fernet
+        password = input("Enter your password for password:\n").encode()
+
+        print("Are you happy with the following, ENTER (y) for YES or (n) for NO?\n")
+        print(login.decode() + ' as login ' + password.decode() + ' as password')
+
+        happy = input()
+        
+        if(happy == 'y' or happy == 'Y' or happy == 'YES' or happy == 'Yes'):
+            create_loop = False
+
+        else:
+            print("Ok, try another login password combination.")
+
+    key = Fernet.generate_key() # make the unique user key
+
+    with open('filekey.key', 'wb') as filekey: # store the encrypted user key to the file
+        filekey.write(key)
+
+def get_key():                                      # returns the key to the user interface
+
+    with open('filekey.key', 'rb') as filekey:
+                key = filekey.read()
+                f = Fernet(key)
+    
+    return f
+
+
+
+  
+
