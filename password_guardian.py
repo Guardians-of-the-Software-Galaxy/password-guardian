10cc04749c7853b401f25cbc21b1a77a7df0b325
nicholasbcarr <89881148+nicholasbcarr@users.noreply.github.com>
2023-02-21 19:31:09 -1000

    cleaned for assignment 2

--- password_guardian.py
+++ password_guardian.py
@@ -1,20 +1,19 @@
 
 from password_guardian_driver import *
 
-
-
 if(login_request()):
-    # read data.txt into a list
-    print("Hello big butt!")
-    # call function to read encrypted data from data.txt into a list or array of credential objects !LEAVE ENCRYPTED!
+    print("Welcome to Password Guardian, you have sucessfully logged in!")
+    # store the app credential first in the array or list to check passwords for content modification
+    # call function to read encrypted data from data.txt into a list or array of credential objects 
+    # access the array of credential objects based on application name
+    # add or delete credential objects to array/list 
+    # time out the user_interface loop
+    # close the program option
 
 else:
     print("Would you like to create a password guardian? Enter (y) for YES, (n) for NO")
     create_account()
 
-f = get_key()
-message = f.encrypt(b'Hello big butt!')
-print(message)
-print(f.decrypt(message))
+
         
 
